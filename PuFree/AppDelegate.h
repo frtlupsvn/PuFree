ab5eb3da5f551f95b9f285e5f57a982f06e1ab96
Mac OS X <macosx@Macs-MacBook-Pro.local>
2014-03-28 23:06:04 +0700

    Initial Commit

---
+++ PuFree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PuFree
+//
+//  Created by Mac OS X on 3/28/14.
+//  Copyright (c) 2014 Zoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PuFree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PuFree
+//
+//  Created by Mac OS X on 3/28/14.
+//  Copyright (c) 2014 Zoom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
